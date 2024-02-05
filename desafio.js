eba21fcd1659cc34c5f50dc6179ffd73166bdf2c
thalesreis7 <thales.reisfurtado@gmail.com>
2024-02-05 20:40:45 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,171 @@
+// 01 - crie uma condição composta com uma variável dia e a condição verifica 
+// se está de dia mostre 'claro' senão mostre está 'de noite':
+
+// let dia = true;
+
+// if(dia == true){
+//     console.log("claro");
+// }else{
+//     console.log("noite");
+// }
+
+
+
+
+// 02 - Crie um loop for() que exiba apenas números pares até o 20 no console.log():
+
+// console.log("Início do programa: ");
+
+// for(i = 0; i <= 20; i++){
+//     if(i%2 === 0){
+//         console.log("É par: " + i);
+//     }
+// }
+
+// console.log("Programa finalizado! ");
+
+
+
+
+
+// 03 - crie uma função que exiba uma mensagem no console:
+
+// function mensagem(){
+//     console.log("#Confianoprocesso ");
+// }
+
+// mensagem();
+
+
+
+
+
+
+// 04 - crie uma função que receba o seu nome como (parâmetro) e exiba no console:
+
+// function nome(nome){
+//     console.log(nome);
+// }
+
+// nome("Thales");
+
+
+
+
+// 05 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console:
+
+// function perfil(nome, idade, musica){
+//     console.log(nome + " tem " + idade + " anos e gosta de " + musica);
+
+// }
+
+// perfil("Thales", 26, "Rap");
+
+
+
+
+
+// 06 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+// function enterterimento (filme, música){
+//     console.log("Filme: " + filme + "\nMúsica : " +  música);
+// }
+
+// enterterimento("Vingadores Ultimato", "Eminem - Lose Yourself");
+
+
+
+
+// 07 - crie uma função que retorne o triplo do número recebido no parâmetro da função:
+
+// function triplo(numero){
+//     return numero * 3;
+// };
+
+// console.log(triplo(3));
+
+
+
+
+
+
+// // 08 - crie uma função que  verifique se uma  variável é true ou false:
+
+// verifica = ()=>{
+//     let v = 1;
+//     if(v === 0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+
+// verifica();
+
+
+
+
+
+
+// 09 - Crie um array que receba 5 itens e exiba no console:
+
+// let series = ["The Walking Dead", " Game of Thrones", " Loki", " Freinds", " Breaking Bad."];
+//     console.log("Séries: "  + series);
+
+
+
+
+
+
+// 10 - Utilize um método para adicionar um nome ao inicio do array.
+
+// let name = ["João", " Carlos", " Raquel"];
+// name.unshift("Isabele")
+
+// console.log(name);
+
+
+
+// 11 - Utilize um método para remover o último nome do array.
+
+// let name = ["Isabele", "João", "Carlos", "Raquel", ];
+// name.pop(name);
+
+// console.log(name);
+
+
+
+// 12 - Utilize um método para adicionar dois nomes ao fim do array.
+
+// let name = ["Isabele", "João", "Carlos", "Raquel", ];
+// name.push("Marcos", "Maria");
+
+// console.log(name);
+
+
+
+
+// 13 - Utilize um método para remover o primeiro nome do array.
+
+// let name = ["Isabele", "João", "Carlos", "Raquel", "Marcos", "Maria" ];
+// name.shift();
+
+// console.log(name);
+
+
+// 14 - Utilize um método para adicionar no meio deste array.
+// let nome = ["João",  "Maria",  "Jose",  "Pedro"];
+
+// nome.splice(2, 0, "Thales");
+
+// console.log(nome);
+
+
+
+// 15 - Utilize um método para organizar em ordem crescente o seguinte array:
+
+//  let numbers = [7,5,6,3,8,9,2,1,4];
+
+//  numbers.sort();
+
+//  console.log(numbers);
